5554c4d5ae20a3320b7eaae4acd4074963876082
Zhong <893646331@qq.com>
2017-11-22 23:15:37 +0800

    string

---
+++ src/string/DetectCapital.java
@@ -0,0 +1,15 @@
+package string;
+
+public class DetectCapital {
+    public boolean detectCapitalUse(String word) {
+        int numUpper = 0;
+        for (int i=0;i<word.length();i++)
+            if (Character.isUpperCase(word.charAt(i))) numUpper++;
+        if (numUpper == 1) return Character.isUpperCase(word.charAt(0));
+        return numUpper == 0 || numUpper == word.length();
+    }
+
+    public static void main(String... args) {
+        System.out.print(Character.isUpperCase('\u0001'));
+    }
+}
---
+++ src/string/PalindromicSubstrings.java
@@ -0,0 +1,36 @@
+package string;
+
+public class PalindromicSubstrings {
+    //dp
+    public int countSubstrings(String s) {
+        int n = s.length();
+        int res = 0;
+        boolean[][] dp = new boolean[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]);
+                if(dp[i][j]) ++res;
+            }
+        }
+        return res;
+    }
+
+    int count = 0;
+
+    public int countSubstrings2(String s) {
+        if (s == null || s.length() == 0) return 0;
+
+        for (int i = 0; i < s.length(); i++) { // i is the mid point
+            extendPalindrome(s, i, i); // odd length;
+            extendPalindrome(s, i, i + 1); // even length
+        }
+
+        return count;
+    }
+
+    private void extendPalindrome(String s, int left, int right) {
+        while (left >=0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            count++; left--; right++;
+        }
+    }
+}
