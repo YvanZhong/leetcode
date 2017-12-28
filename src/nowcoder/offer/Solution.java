42a83df9e2e3ae58e893f216e5dee2e1cf8ffbd1
Zhong <zhongyifan@neotel.com.cn>
2017-12-28 09:56:44 +0800

    nowcode

---
+++ src/nowcoder/offer/Solution.java
@@ -0,0 +1,10 @@
+package nowcoder.offer;
+
+/**
+ * Created by Zhong on 2017/12/27.
+ */
+public class Solution {
+    public String replaceSpace(StringBuffer str) {
+        return str.toString().replaceAll(" ", "%20");
+    }
+}
